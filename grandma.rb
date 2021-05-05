5316f3a8b5e5d1aeb2325adec7b5d133911b9f9c
Vincent Tran <vincentuong@yahoo.com>
2021-05-05 03:22:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-       if phrase == "I LOVE YOU GRANDMA!"
-			           "I LOVE YOU TOO PUMPKIN!"
-				elsif phrase != phrase.upcase 
-				         "HUH!? SPEAK UP SONNY!"
-				else
-				         "NO, NOT SINCE 1938!"
+      if phrase == "I LOVE YOU GRANDMA!"
+			"I LOVE YOU TOO PUMPKIN!"
+			elsif phrase != phrase.upcase 
+				  "HUH!? SPEAK UP SONNY!"
+			else
+			"NO, NOT SINCE 1938!"
+			end
 end
